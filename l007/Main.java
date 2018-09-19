51b9fd83c047d6c551bb3c89e47c52f1bb680f23
SunHao <2457302636@qq.com>
2018-09-19 21:20:50 +0800

    www.sunhao.pro

---
+++ l007/Main.java
@@ -0,0 +1,42 @@
+package leetcode.l007;
+
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        boolean flag = true;
+        String str1 = x + "";
+        String str2 = "";
+        boolean isFirst = true;
+        if(str1.charAt(0) == '-') {
+        	flag = false;
+        	str1 = str1.substring(1, str1.length());
+        }
+        for(int i = str1.length() - 1; i >= 0; i--) {
+        	if(isFirst && str1.charAt(i) == '0') continue;
+        	isFirst = false;
+        	str2 += str1.charAt(i);
+        }
+        if(str2 == null || str2 == "") {
+        	return 0;
+        } else {
+	        long temp = Long.parseLong(str2);
+	        if(temp > Integer.MAX_VALUE) {
+	        	result = 0;
+	        } else {
+	        	result = (int)temp;
+	        	if(!flag) result = -result;
+	        }
+	        return result;        	
+        }
+    }
+}
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int x = 123;
+		System.out.println(solution.reverse(x));
+	}
+	
+}
---
+++ l009/Main.java
@@ -0,0 +1,25 @@
+package leetcode.l009;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = x + "";
+        for(int i = 0; i <= str.length() / 2; i++) {
+        	if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        		//System.out.println(str.charAt(i) + " " + str.charAt(str.length() - 1 - i));
+        		return false;
+        	}
+        }
+        return true;
+    }
+}
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int x = 135;		
+		System.out.println(solution.isPalindrome(x));
+	}
+
+	
+}
