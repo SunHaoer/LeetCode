46f1d2683cf59f2ec206816742b71cca08c7b4db
SunHao <2457302636@qq.com>
2018-11-28 20:40:51 +0800

    www.sunhao.pro

---
+++ l0028/Main.java
@@ -0,0 +1,18 @@
+package leetcode.l0028;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+    	return 0;
+    }
+}
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		String haystack = "aaaaa";
+		String needle = "bba";
+		solution.strStr(haystack, needle);
+	}
+	
+}
